680cbefdc7a72187e2cd471512db5f3d94ab3140
Kirill <urmancheev.it@gmail.com>
2025-06-04 00:41:52 +0300

    test: check Netlify auto-deploy

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,15 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      {' '}
+      <div>
+        <h1>Redux Todo App</h1>
+        <p style={{ color: 'red', fontWeight: 'bold' }}>
+          ({new Date().toLocaleString()})
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default App;
